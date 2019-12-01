52ca293ea815199e32db1ebfd17fc4c9017e6dc8
Levi Roth <levimroth@gmail.com>
2019-12-01 07:49:54 -0500

    Day 1, part 2

--- src/day01.ml
+++ src/day01.ml
@@ -7,13 +7,14 @@ include Solution.Day.Make (struct
   module Common = struct
     module Input = Converters.Int_list
     module Output = Int
+
+    let fuel_requirement n = (n / 3) - 2
   end
 
   module Part_1 = Solution.Part.Make (struct
     include Common
 
     let one_based_index = 1
-    let fuel_requirement n = (n / 3) - 2
     let solve = List.sum (module Int) ~f:fuel_requirement
   end)
 
@@ -27,5 +28,33 @@ include Solution.Day.Make (struct
     33583 |}]
   ;;
 
-  let parts : (module Solution.Part.S) list = [ (module Part_1) ]
+  module Part_2 = Solution.Part.Make (struct
+    include Common
+
+    let one_based_index = 2
+
+    let fuel_requirement_accounting_for_extra n =
+      let rec aux n total =
+        let required_fuel = fuel_requirement n in
+        match required_fuel <= 0 with
+        | true -> total
+        | false -> aux required_fuel (total + required_fuel)
+      in
+      aux n 0
+    ;;
+
+    let solve = List.sum (module Int) ~f:fuel_requirement_accounting_for_extra
+  end)
+
+  let%expect_test "Part 1" =
+    let part_1_test_cases = [ "12"; "14"; "1969"; "100756" ] in
+    List.iter part_1_test_cases ~f:(Fn.compose print_endline Part_2.solve_input);
+    [%expect {|
+    2
+    2
+    966
+    50346 |}]
+  ;;
+
+  let parts : (module Solution.Part.S) list = [ (module Part_1); (module Part_2) ]
 end)
