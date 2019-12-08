3bada27c3713ab50788b9c8628e1ba302585e87c
Levi Roth <levimroth@gmail.com>
2019-12-08 12:42:52 -0500

    Solution for day 6, part 2

--- src/day06.ml
+++ src/day06.ml
@@ -38,7 +38,7 @@ let count_orbits graph =
   aux "COM" 0
 ;;
 
-let%expect_test _ =
+let%expect_test "Part 1" =
   let graph =
     {|
 COM)B
@@ -59,7 +59,61 @@ K)L
   print_s [%message (graph : string list String.Map.t)];
   let count = count_orbits graph in
   print_s [%message (count : int)];
-  [%expect]
+  [%expect{|
+    (graph
+     ((B (C G)) (C (D)) (COM (B)) (D (E I)) (E (F J)) (G (H)) (J (K)) (K (L))))
+    (count 42) |}]
+;;
+
+let undirected_graph_of_input (input : Input.t) =
+  List.concat_map input ~f:(fun { center; orbiter } ->
+      [ center, orbiter; orbiter, center ])
+  |> String.Map.of_alist_multi
+;;
+
+let find_distance graph start stop =
+  let rec loop frontier next_frontier visited depth =
+    match frontier with
+    | [] -> loop next_frontier [] visited (depth + 1)
+    | hd :: tl ->
+      (match String.equal hd stop with
+      | true -> depth
+      | false ->
+        let neighbors =
+          Map.find_exn graph hd |> List.filter ~f:(Fn.non (Set.mem visited))
+        in
+        loop
+          tl
+          (neighbors @ next_frontier)
+          (List.fold neighbors ~init:visited ~f:Set.add)
+          depth)
+  in
+  loop [ start ] [] String.Set.empty 0 - 2
+;;
+
+let%expect_test "Part 2" =
+  let graph =
+    {|
+      COM)B
+      B)C
+      C)D
+      D)E
+      E)F
+      B)G
+      G)H
+      D)I
+      E)J
+      J)K
+      K)L
+      K)YOU
+      I)SAN
+    |}
+    |> Input.of_string
+    |> undirected_graph_of_input
+  in
+  let count = find_distance graph "YOU" "SAN" in
+  print_s [%message (count : int)];
+  [%expect{| (count 4) |}]
 ;;
 
 module Part_1 = Solution.Part.Make (struct
@@ -70,7 +124,19 @@ module Part_1 = Solution.Part.Make (struct
   let solve = Fn.compose count_orbits graph_of_input
 end)
 
+module Part_2 = Solution.Part.Make (struct
+  module Input = Input
+  module Output = Int
+
+  let one_based_index = 2
+
+  let solve input =
+    let graph = undirected_graph_of_input input in
+    find_distance graph "YOU" "SAN"
+  ;;
+end)
+
 include Solution.Day.Make (struct
   let day_of_month = 6
-  let parts : (module Solution.Part.S) list = [ (module Part_1) ]
+  let parts : (module Solution.Part.S) list = [ (module Part_1); (module Part_2) ]
 end)
