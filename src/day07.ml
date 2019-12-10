7a2d2936a01c4243e107b3593c2f6085db3494a2
Levi Roth <levimroth@gmail.com>
2019-12-09 22:18:14 -0500

    Solution for day 7, part 2

--- src/day07.ml
+++ src/day07.ml
@@ -69,7 +69,90 @@ module Part_1 = Solution.Part.Make_async (struct
   ;;
 end)
 
+let part_2_sequences = List.range 5 10 |> permutations
+
+let run_sequence program sequence =
+  let last_value = ref None in
+  let input_pipes =
+    List.map sequence ~f:(fun phase ->
+        let reader, writer = Pipe.create () in
+        Pipe.write_without_pushback writer phase;
+        reader, writer)
+  in
+  let _, first_writer = List.hd_exn input_pipes in
+  Pipe.write_without_pushback first_writer 0;
+  let output_pipes =
+    List.length sequence |> List.init ~f:(fun (_ : int) -> Pipe.create ())
+  in
+  let return_to_front, send_to_thrusters =
+    let reader, _writer = List.last_exn output_pipes in
+    Pipe.fork reader ~pushback_uses:`Fast_consumer_only
+  in
+  don't_wait_for (Pipe.transfer_id return_to_front first_writer);
+  let rec connect pipes =
+    match pipes with
+    | [] | [ _ ] -> return ()
+    | (_, (output_reader, _)) :: (((_, input_writer), _) :: _ as rest) ->
+      don't_wait_for (Pipe.transfer_id output_reader input_writer);
+      connect rest
+  in
+  let pipes = List.zip_exn input_pipes output_pipes in
+  let%bind () = connect pipes in
+  let%bind () =
+    Deferred.List.iteri
+      pipes
+      ~how:`Parallel
+      ~f:(fun i ((input_reader, _), (_, output_writer)) ->
+        Intcode.run_program ~id:i program ~input:input_reader ~output:output_writer
+        |> Deferred.ignore_m)
+  in
+  let%bind () =
+    Pipe.iter send_to_thrusters ~f:(fun value ->
+        last_value := Some value;
+        return ())
+  in
+  return (Option.value_exn !last_value)
+;;
+
+let%expect_test "Part 2" =
+  let inputs =
+    [ ( "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,\n\
+         27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+      , [ 9; 8; 7; 6; 5 ] )
+    ; ( "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,\n\
+         -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,\n\
+         53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+      , [ 9; 7; 8; 5; 6 ] )
+    ]
+  in
+  let%bind () =
+    Deferred.List.iter inputs ~f:(fun (program, sequence) ->
+        let program = Intcode.Input.of_string program in
+        let%bind output = run_sequence program sequence in
+        print_s [%message (output : int)];
+        return ())
+  in
+  [%expect {|
+    (output 139629729)
+    (output 18216) |}]
+;;
+
+module Part_2 = Solution.Part.Make_async (struct
+  module Input = Intcode.Input
+  module Output = Int
+
+  let one_based_index = 2
+
+  let solve program =
+    let%bind opt =
+      Deferred.List.map part_2_sequences ~f:(run_sequence program)
+      >>| List.max_elt ~compare
+    in
+    return (Option.value_exn opt)
+  ;;
+end)
+
 include Solution.Day.Make (struct
   let day_of_month = 7
-  let parts = [ Part_1.command ]
+  let parts = [ Part_1.command; Part_2.command ]
 end)
