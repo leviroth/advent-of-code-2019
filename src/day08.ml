87f1c474d35be00ce2b3ceda8bd66b5fd6d365c8
Levi Roth <levimroth@gmail.com>
2019-12-09 23:05:01 -0500

    Solution for day 8, part 2

--- src/day08.ml
+++ src/day08.ml
@@ -25,6 +25,54 @@ let checksum layer =
   List.count layer ~f:(Char.equal '1') * List.count layer ~f:(Char.equal '2')
 ;;
 
+let align_layers input ~width ~height =
+  let input = String.to_list input in
+  let size = width * height in
+  List.init size ~f:(fun bucket ->
+      List.filteri input ~f:(fun i _elt -> i mod size = bucket))
+;;
+
+module Color = struct
+  type t =
+    | Black
+    | White
+    | Transparent
+
+  let of_char char =
+    match char with
+    | '0' -> Black
+    | '1' -> White
+    | '2' -> Transparent
+    | _ -> raise_s [%message "Unknown color char" (char : char)]
+  ;;
+
+  let to_print_char t =
+    match t with
+    | Black -> '#'
+    | White -> ' '
+    | Transparent -> '2'
+  ;;
+end
+
+let color_of_pixel (pixel : Color.t list) =
+  List.rev pixel
+  |> List.fold ~init:Color.White ~f:(fun acc color ->
+         match color with
+         | Black -> Black
+         | White -> White
+         | Transparent -> acc)
+;;
+
+let solve_part_2 input ~width ~height =
+  align_layers input ~width ~height
+  |> List.map ~f:(List.map ~f:Color.of_char)
+  |> List.map ~f:color_of_pixel
+  |> List.map ~f:Color.to_print_char
+  |> List.chunks_of ~length:width
+  |> List.map ~f:String.of_char_list
+  |> String.concat ~sep:"\n"
+;;
+
 module Part_1 = Solution.Part.Make (struct
   module Input = Input
   module Output = Int
@@ -33,7 +81,22 @@ module Part_1 = Solution.Part.Make (struct
   let solve input = layers input ~width ~height |> layer_with_fewest_zeros |> checksum
 end)
 
+module Part_2 = Solution.Part.Make (struct
+  module Input = Input
+  module Output = String
+
+  let one_based_index = 2
+  let solve = solve_part_2 ~width ~height
+end)
+
+let%expect_test "Part 2" =
+  solve_part_2 "0222112222120000" ~width:2 ~height:2 |> print_endline;
+  [%expect{|
+    #
+     # |}]
+;;
+
 include Solution.Day.Make (struct
   let day_of_month = 8
-  let parts = [ Part_1.command ]
+  let parts = [ Part_1.command; Part_2.command ]
 end)
